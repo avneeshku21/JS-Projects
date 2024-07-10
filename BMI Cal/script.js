f1b7a79530de86ed19cce3b31ad12ff3747e43fd
Avneesh Kumar <kaviikumar010@gmail.com>
2024-07-10 15:25:59 +0530

    update

--- BMI Cal/script.js
+++ BMI Cal/script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('form');
+const p=document.querySelector('p')
 // this usecase will give you empty
 // const height = parseInt(document.querySelector('#height').value)
 
@@ -11,12 +12,27 @@ form.addEventListener('submit', function (e) {
 
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
+
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight ${weight}`;
-  } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  } else  {
+    let bmi = (weight / ((height * height) / 10000)).toFixed(2);// it give 2 decimal place value
     //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
-  }
+    // results.innerHTML = `<span>${bmi}</span>`;
+
+  
+
+  if(bmi>18.6)
+    
+    results.innerHTML=`Under Weight: <span>${bmi}</span>`;
+    
+    else if (bmi >= 18.6 && bmi < 24.9) 
+        result.innerHTML = 
+            `Normal : <span>${bmi}</span>`;
+
+    else result.innerHTML =
+        `Over Weight : <span>${bmi}</span>`;
+}
 });
+
 
